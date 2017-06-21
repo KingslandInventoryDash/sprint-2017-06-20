4de2a5125ab5aff46d6ca6b0e2d38e6413529a01
waskerdu <rileywestonmiller@outlook.com>
2017-06-21 13:05:55 -0700

    uploaded my code

---
+++ InventoryDash/Models/ingredientEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryDash.Models
+{
+    public class ingredientEntry
+    {
+        public int sandwichId { get; set; }
+        public int ingredientId { get; set; }
+    }
+}
